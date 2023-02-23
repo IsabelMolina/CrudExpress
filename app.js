7e1facc990e7248951d489107326124379635e11
IsabelMolina <isabelcmolina12@gmail.com>
2023-02-23 13:53:10 -0500

    cambios

--- app.js
+++ app.js
@@ -1,44 +1,21 @@
-const express = require("express")
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const petsRoute = require('./router/pets');
+const port = 3000;
+
+const uri = `mongodb+srv://vet-usr:vet-pwd@cluster0.zd7k6.mongodb.net/vet?retryWrites=true&w=majority`
+mongoose.connect(uri, { useUnifiedTopology: true });
+
 const app = express();
 
-const bodyParser = require("body-parser");
-
-// Middleware
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json())
-
-require('dotenv').config();
-
-const port = process.env.PORT || 3000;
-
-// conexion base de datos
-const mongoose = require("mongoose");
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.zd7k6.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
-
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=>console.log("Base de datos conectada"))
-.catch(e=>console.log(e))
-
-// motor de plantillas
 app.set("view engine", "ejs");
 app.set("views", __dirname+"/views");
 
-// Middleware (accion que se ejecuta antes de nuestra funcion de ruta)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(express.static(__dirname + "/public"));
-
-app.use('/', require('./router/web'));
-app.use('/mascotas', require('./router/mascotas'));
-
-app.use((req, res, next) => {
-  // res.status(404).send("Sorry cant find that!");
-  // res.status(404).sendFile(__dirname + "/public/404.html");
-  res.status(404).render("404", {
-    titulo: 404,
-    descripcion: "Pagina no encontrada"
-  });
-});
+app.use('/pets', petsRoute);
 
 app.listen(port, () => {
   console.log('Servidor a su servicio en el puerto', port);
---
+++ router/pets.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const router = express.Router();
+
+const Pet = require('../models/Pet')
+
+router.get('/', async(req, res) => {
+
+  const pets = await Pet.find();
+
+  res.render("pets/index", { 
+    pets: pets
+  })      
+
+})
+
+router.get('/create', (req, res)=>{
+
+  res.render('pets/create');
+
+})
+
+router.post('/', async (req, res)=>{
+
+  const body =  req.body;
+
+  if(!body.name || !body.description){
+    res.sendStatus(422);
+  }
+
+  await Pet.create(body);
+
+  res.redirect("/pets");
+
+})
+
+router.get('/:id', async(req, res) => {
+
+  const id = req.params.id;
+
+  try {
+    const pet = await Pet.findOne({_id: id})
+    res.render('pets/detail', {
+      pet: pet
+    })
+  } 
+  catch (error) {
+    res.sendStatus(404)
+  }
+
+})
+
+router.delete("/:id", async (req, res) => {
+
+  const id = req.params.id;
+
+  try {
+    await Pet.findByIdAndDelete({ _id: id });
+    res.sendStatus(204)
+  } 
+  catch (error) {
+    res.sendStatus(404)
+  }
+})
+
+router.put("/:id", async(req, res)=>{
+
+  const id = req.params.id;
+  const body = req.body;
+
+  if(!body.name || !body.description){
+    res.sendStatus(422);
+  }
+
+  try {
+    await Pet.findByIdAndUpdate(id, body, { useFindAndModify: false });
+    res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(404);
+  }
+})
+
+module.exports = router;
